199dab710c9338e85c61f855af6307c083546cf3
Indraniel <indraniel@gmail.com>
2013-12-14 22:24:09 -0600

    + addition of an atom feed
      Based on instructions found here:
    
      http://flask.pocoo.org/snippets/10/

--- _annex/views.py
+++ _annex/views.py
@@ -1,12 +1,17 @@
-from flask import render_template, url_for, redirect
+from flask import render_template, url_for, redirect, request
 from app import app, pages
+from werkzeug.contrib.atom import AtomFeed
 
-@app.route('/')
-def home():
+def sorted_posts():
     posts = [page for page in pages if 'date' in page.meta]
     sorted_posts = sorted(posts, reverse=True,
                                  key=lambda page: page.meta['date'] )
-    return render_template('index.html', pages=sorted_posts)
+    return sorted_posts
+
+@app.route('/')
+def home():
+    posts = sorted_posts()
+    return render_template('index.html', pages=posts)
 
 @app.route('/<path:path>/')
 def page(path):
@@ -19,3 +24,18 @@ def page(path):
 def about():
     page = pages.get_or_404('pages/about')
     return render_template('page.html', page=page)
+
+@app.route('/all.rss.xml')
+def rss_feed():
+    feed = AtomFeed('Articles', feed_url=request.url, url=request.url_root)
+    pages = sorted_posts()
+    for page in pages:
+        feed.add(
+            page.meta['title'],
+            page,
+            content_type='html',
+            author=page.meta.get('author', 'indraniel'),
+            url='/'.join([app.config['FREEZER_BASE_URL'], page.path]),
+            updated=page.meta['date'],
+        )
+    return feed.get_response()
